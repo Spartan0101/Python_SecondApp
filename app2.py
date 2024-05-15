e320af07f00c1ca8b9fedc369ffe21e42e35271a
Spartan1010 <tranulovianos@gmail.com>
2024-05-15 12:34:30 +0100

    first commit

--- app2.py
+++ app2.py
@@ -1,1 +1,19 @@
-print ("test v2")
+#Tip calculator
+
+#print("Welcome to the Tip calculator!"[1])
+
+#number = input()
+#a = int(number[0])
+#b = int(number[1])
+#print(a + b)
+
+#Body mass index calculator
+
+height = input()
+weight = input()
+weight_as_int = int(weight)
+height_as_float = float(height)
+bmi = weight_as_int / (height_as_float * height_as_float)
+
+bmi_as_int = int(bmi)
+print(bmi_as_int)
